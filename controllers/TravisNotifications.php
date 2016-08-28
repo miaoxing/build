16ec388609381bc19d45dce16d032794f4424809
twinh <twinhuang@qq.com>
2016-08-28 15:46:46 +0800

    增加webhook日志 #1535

--- controllers/TravisNotifications.php
+++ controllers/TravisNotifications.php
@@ -4,33 +4,38 @@ namespace miaoxing\build\controllers;
 
 use miaoxing\build\services\TravisNotification;
 use Wei\BaseController;
+use Wei\Logger;
 use Wei\Request;
 use Wei\Response;
 
 /**
  * @property TravisNotification $travisNotification
+ * @property Logger $logger
  */
 class TravisNotifications extends BaseController
 {
     public function createAction(Request $req, Response $res)
     {
         $notify = $this->travisNotification;
-
-        echo $this->request->getContent() . PHP_EOL;
-
         if (!$notify->isValidRequest()) {
-            return $res->setContent('Invalid authorization')->setStatusCode(401);
+            return $this->response('Invalid authorization', 401);
         }
 
-        $repoSlug = $req->getServer('HTTP_TRAVIS_REPO_SLUG');
         $payload = json_decode($req['payload'], true);
-        echo 'Received valid payload for repository ' . $repoSlug . PHP_EOL;
-
         if (!$notify->isFailed($payload['status_message'])) {
-            return $res->setContent('Build ' . $payload['status']);
+            return $this->response('Build ' . $payload['status']);
         }
 
         $http = $notify->createIssue($payload);
-        return $res->setContent($http->getResponseText())->setStatusCode($http->getCurlInfo(CURLINFO_HTTP_CODE));
+        return $this->response($http->getResponseText(), $http->getCurlInfo(CURLINFO_HTTP_CODE));
+    }
+
+    protected function response($content, $status = 200)
+    {
+        $level = in_array($status, [200, 401]) ? 'info' : 'warning';
+        $this->logger->log($level, $content);
+
+        $this->response->setContent($content)->setStatusCode($status);
+        return $this->response;
     }
 }
