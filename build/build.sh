fe3016db4fabd5804fe4df2f06b4807eb2fff51d
twinh <twinhuang@qq.com>
2016-11-22 21:09:09 +0800

    尝试通过外部脚本安装 #2028

--- build/build.sh
+++ build/build.sh
@@ -10,11 +10,11 @@ command="vendor/bin/phpcs --report-file=${report} ."
 ${command}
 code=$?
 
-# 4. 移除最后两行空白,并附加命令到报告中
+# 2. 移除最后两行空白,并附加命令到报告中
 head -n -2 "${report}" > temp.txt ; mv temp.txt "${report}"
 append_report "${report}" "${command}"
 
-# 5. 如果检测到问题,仍然认为是运行成功
+# 3. 如果检测到问题,仍然认为是运行成功
 if [[ code -eq 1 ]]; then
   exit 0
 else
