b40581cee3214e3eef27e857541cb76016af5379
twinh <twinhuang@qq.com>
2016-11-22 16:37:39 +0800

    增加专门的report目录

--- build/build.sh
+++ build/build.sh
@@ -3,7 +3,7 @@
 source "${BASH_SOURCE[0]%/*}/base.sh"
 
 # 1. 生成不能处理的文件的报告
-report="reports/phpcs.txt"
+report="build/reports/phpcs.txt"
 command="php phpcs.phar --report-file=${report} ."
 ${command}
 code=$?
