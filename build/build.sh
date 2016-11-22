142c794aa01d1b0b91b3dfe6c7f6ef5661c30b9c
twinh <twinhuang@qq.com>
2016-11-22 16:31:01 +0800

    尝试增加phpcs检查

---
+++ build/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+source "${BASH_SOURCE[0]%/*}/base.sh"
+
+# 1. 生成不能处理的文件的报告
+report="build/reports/phpcs.txt"
+command="phpcs --report-file=${report} ."
+${command}
+code=$?
+
+# 4. 移除最后两行空白,并附加命令到报告中
+head -n -2 "${report}" > temp.txt ; mv temp.txt "${report}"
+append_report "${report}" "${command}"
+
+# 5. 如果检测到问题,仍然认为是运行成功
+if [[ code -eq 1 ]]; then
+  exit 0
+else
+  exit ${code}
+fi
