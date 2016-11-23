caca9aec3a5e3cc7302a0101715afde784b5c14b
twinh <twinhuang@qq.com>
2016-11-23 20:45:05 +0800

    phpunit移到build中 #2028

--- build/build-travis.sh
+++ build/build-travis.sh
@@ -2,7 +2,9 @@
 
 # 1. 执行各类检查
 mkdir -p reports
-#bash "${BASH_SOURCE[0]%/*}/phpcs.sh"
+
+bash "${BASH_SOURCE[0]%/*}/phpunit.sh"
+bash "${BASH_SOURCE[0]%/*}/phpcs.sh"
 bash "${BASH_SOURCE[0]%/*}/phpmd.sh"
 bash "${BASH_SOURCE[0]%/*}/csslint.sh"
 bash "${BASH_SOURCE[0]%/*}/eslint.sh"
