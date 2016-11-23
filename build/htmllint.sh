8b5ab09c42c66ae0a771672735869b2d3a80ce4a
twinh <twinhuang@qq.com>
2016-11-23 12:04:17 +0800

    增加htmllint检查 #2028

---
+++ build/htmllint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source "${BASH_SOURCE[0]%/*}/base.sh"
+
+# 1. 执行命令
+report="reports/htmllint.txt"
+command="htmllint htmllint resources/views/*/*"
+${command} | tee ${report}
+
+# 3. 调整报告
+if [[ ${PIPESTATUS[0]} == 0 ]]; then
+  # 如果检测没有问题,删除报告
+  rm -f ${report}
+else
+  append_report "${report}" "${command}"
+fi
